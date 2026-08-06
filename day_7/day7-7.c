7660787e7c5c9cfac42679c30b0530b7c0addbfe
Himanshu Verma <hxverma.dev@gmail.com>
2026-08-06 17:27:18 +0530

    Print largest number among all from do while loop

---
+++ day_7/day7-7.c
@@ -0,0 +1,19 @@
+// Print all numbers between a and b that are divisible by 7
+#include <stdio.h>
+int main(){
+	int i = 23;
+	while(i <= 96){
+		if(i % 7 == 0){
+			printf("%d " , i);
+		}else{
+			
+		}
+		i++;
+	}
+
+
+
+
+
+	return 0;
+}
