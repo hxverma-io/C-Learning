bdaa44938b0469a62477d6379b0f0c6865751be7
Himanshu Verma <hxverma.dev@gmail.com>
2026-08-10 10:35:40 +0530

    Print stars in even numbers

---
+++ day_11/day_11-7.c
@@ -0,0 +1,21 @@
+// Print stars in even numbers
+#include <stdio.h>
+int main(){
+	int i = 1;
+	int n = 5
+	while(i <= n){
+		int number = i * 2;
+		int v = 1;
+		while(v <=number){
+			printf("*");
+			v++;
+		}
+		printf("\n");
+		i++;
+	}
+
+
+
+
+	return 0;
+}
