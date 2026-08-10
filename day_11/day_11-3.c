dbea3f686d6f691ae49b15a45ac8cd366550ef00
Himanshu Verma <hxverma.dev@gmail.com>
2026-08-10 09:22:53 +0530

    Print n stars on same line

---
+++ day_11/day_11-3.c
@@ -0,0 +1,14 @@
+//Print n stars on same line
+#include <stdio.h>
+int main(){	
+	char star[] = "*";
+	int length = 8;
+	int i = 1;
+	while(i <= length){
+		printf("%s" , star);
+		i++;
+	}
+
+
+	return 0;
+}
