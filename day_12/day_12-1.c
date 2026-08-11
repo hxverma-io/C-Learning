c2aa446af53c850473f52b526daa64dcc1e84b18
Himanshu Verma <hxverma.dev@gmail.com>
2026-08-11 12:36:03 +0530

    add a , b using function

---
+++ day_12/day_12-1.c
@@ -0,0 +1,13 @@
+// add int a and int b using function
+#include <stdio.h>
+  int add(int a , int b){
+  	int result = a + b;
+  	return result;
+  }
+ int main(){
+	int sum = add(50,90);
+	printf("%d" , sum);
+
+
+ 	return 0;
+ }
