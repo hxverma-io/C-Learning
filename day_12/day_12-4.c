da4e0cb3639f92d58304c6c4096c3bb374bbfc45
Himanshu Verma <hxverma.dev@gmail.com>
2026-08-11 12:55:28 +0530

    Function that returns the square of a number

---
+++ day_12/day_12-4.c
@@ -0,0 +1,15 @@
+// Write a function that returns the square of a number
+#include <stdio.h>
+int square(int n){
+	return n*n;
+}
+int main(){
+	int result = square(20);
+	printf("%d" , result);
+
+
+
+
+
+	return 0;
+}
