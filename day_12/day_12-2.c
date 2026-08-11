28b9e6fd92cb5980782afe447985155e50c89a0d
Himanshu Verma <hxverma.dev@gmail.com>
2026-08-11 12:44:09 +0530

    Multiply 2 numbers using function

---
+++ day_12/day_12-2.c
@@ -0,0 +1,14 @@
+// Multiply a and b using loop
+#include <stdio.h>
+  int multi(int a , int b){
+  	return a * b ;
+  }
+ int main(){
+	int result = multi(40 , 90);
+	printf("%d" , result);
+
+
+
+
+ 	return 0;
+ }
