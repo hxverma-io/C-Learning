fd5ce08061bf4ae2e5195ca44a2cbe23e4cace6e
hxverma-io <hxverma.dev@gmail.com>
2026-08-15 17:29:11 +0530

    Take input in array

---
+++ day_17/day_17-3.c
@@ -0,0 +1,15 @@
+// Take input in array
+#include <stdio.h>
+int main(){
+    int n[5];
+    int i = 0;
+    while(i < 5){
+        scanf("%d" ,&n[i]);
+        i++;
+    }
+
+
+
+
+    return 0;
+}   
